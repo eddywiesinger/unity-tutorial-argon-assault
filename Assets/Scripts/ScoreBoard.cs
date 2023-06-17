d05aa336bb217021669f074de625ce7db1efb5b4
Eddy <wiesinger.edwin@gmail.com>
2023-06-17 19:13:09 +0200

    add score UI with TextMeshPro

--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreBoard : MonoBehaviour
 {
     int score;
+    TMP_Text scoreText;
+
+    private void Start()
+    {
+        scoreText = GetComponent<TMP_Text>();
+        scoreText.SetText(score.ToString());
+    }
     
     public void IncreaseScore(int amountToIncrease) 
     {
         score = score + amountToIncrease;
-        Debug.Log($"Score: {score}");
+        scoreText.SetText(score.ToString());
     }
 }
